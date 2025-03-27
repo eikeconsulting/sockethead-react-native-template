3697a9ff0939cb6a172bbd04569e13a8764afcf3
staimoorali27@gmail.com <staimoorali27@gmail.com>
2025-03-27 16:54:37 +0500

    splash screen setup

--- ios/MyApp/AppDelegate.swift
+++ ios/MyApp/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+// import RNSplashScreen 
 
 @main
 class AppDelegate: RCTAppDelegate {
@@ -13,7 +14,11 @@ class AppDelegate: RCTAppDelegate {
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    // RNSplashScreen.show() // here
+
+    return result
   }
 
   override func sourceURL(for bridge: RCTBridge) -> URL? {
