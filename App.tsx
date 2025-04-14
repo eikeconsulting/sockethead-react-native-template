90e66bc6a5dd5a6cecfca7dea020232c4ff28538
staimoorali27@gmail.com <staimoorali27@gmail.com>
2025-04-14 12:49:51 +0500

    Configure splash for android

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Routes from '@app/routes'
 import { Provider } from "react-redux";
 import { persistor, store } from '@app/store/store';
 import { PersistGate } from "redux-persist/integration/react";
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide()
+    }, 2000);
+  }, [])
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
--- ios/MyApp/AppDelegate.swift
+++ ios/MyApp/AppDelegate.swift
@@ -13,7 +13,11 @@ class AppDelegate: RCTAppDelegate {
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    RNSplashScreen.show() // here
+
+    return result
   }
 
   override func sourceURL(for bridge: RCTBridge) -> URL? {
