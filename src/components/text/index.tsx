03f2280f4f794e3173f70786f13f79044acb6167
staimoorali27@gmail.com <staimoorali27@gmail.com>
2025-04-16 15:37:22 +0500

    handle fonts scaling

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -2,32 +2,41 @@ import React from 'react';
 import { StyleSheet, Text as RNText } from 'react-native';
 import { fontFamily as ff } from '@app/constants';
 import { Colors } from '@app/colors';
+import { RFValue } from "react-native-responsive-fontsize";
+
+interface TextProps {
+    children: any;
+    style?: any;
+    numberOfLines?: number;
+    onPress?: () => void;
+};
 
 const Text = ({ children, style = {}, numberOfLines = 0, onPress }: TextProps) => {
     const defaultFontFamily = ff.regular;
     const defaultFontColor = Colors.textColor;
-
     const flattenedStyle = StyleSheet.flatten(style);
-    const { fontFamily = defaultFontFamily, color = defaultFontColor, ...rest } = flattenedStyle;
 
-    const textProps = {
+    const { fontFamily = defaultFontFamily, color = defaultFontColor, fontSize, ...rest } = flattenedStyle;
+
+    const responsiveFontSize = flattenedStyle?.fontSize ? RFValue(flattenedStyle?.fontSize) : RFValue(12);
+
+    const textStyle = {
+        fontFamily,
+        color,
+        fontSize: responsiveFontSize,
         ...rest,
+    };
+
+    const textProps: any = {
         numberOfLines,
-        style: [{ fontFamily, color }, flattenedStyle],
+        style: textStyle
     };
 
     if (onPress) {
         textProps.onPress = onPress;
     }
 
-    return <RNText {...textProps}>{children}</RNText>;
+    return <RNText maxFontSizeMultiplier={1.5} {...textProps}>{children}</RNText>;
 };
 
 export default Text;
-
-interface TextProps {
-    children: any,
-    style?: any,
-    numberOfLines?: number,
-    onPress?: Function,
-};
--- src/components/textField/index.tsx
+++ src/components/textField/index.tsx
@@ -4,8 +4,17 @@ import Text from '@app/components/text';
 import { Colors } from '@app/colors';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { StyleSheet } from 'react-native';
+import { fontFamily as ff } from '@app/constants';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 const TextField = (props: any) => {
+
+    const defaultFontFamily = ff.regular;
+    const defaultFontColor = Colors.textColor;
+    const flattenedStyle = StyleSheet.flatten(props?.style) || {};
+    const { fontFamily = defaultFontFamily, color = defaultFontColor, fontSize, ...rest } = flattenedStyle;
+    const responsiveFontSize = flattenedStyle?.fontSize ? RFValue(flattenedStyle?.fontSize) : RFValue(12);
+    const textStyle = { fontFamily, color, fontSize: responsiveFontSize, ...rest, };
 
     const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(props?.password);
 
@@ -14,16 +23,18 @@ const TextField = (props: any) => {
     return (
         <>
             <TextInput
+                {...props}
                 mode='outlined'
-                theme={{ roundness: 12, colors: { background: Colors.white, } }}
-                textColor={Colors.black}
+                theme={{ roundness: 12, colors: { background: Colors.white, }, fonts: { bodyLarge: { fontFamily: fontFamily } } }}
+                textColor={color ?? Colors.black}
                 autoCapitalize={props?.password ? "none" : "sentences"}
                 activeOutlineColor={Colors.blueGrey3}
                 outlineColor={Colors.blueGrey3}
                 placeholderTextColor={Colors.textColor}
                 secureTextEntry={props?.password ? isPasswordVisible : false}
                 right={props.password ? <TextInput.Icon onPress={togglePassword} icon={() => isPasswordVisible ? <Ionicons name='eye-off' size={18} color={Colors.blueGrey3} /> : <Ionicons name="eye" size={18} color={Colors.blueGrey3} />} /> : null}
-                {...props}
+                maxFontSizeMultiplier={1.2}
+                style={{ ...textStyle }}
             />
             {props?.error && <Text style={styles.errorText}>{props?.error}</Text>}
         </>
